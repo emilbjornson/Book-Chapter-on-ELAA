978b154a055096bede22b805beca8da208976748
parisaramezani <116488979+parisaramezani@users.noreply.github.com>
2022-12-21 00:48:46 +0100

    Update nearfield_multiplexing.m

--- nearfield_multiplexing.m
+++ nearfield_multiplexing.m
@@ -55,15 +55,20 @@ for m = 1:length(zRange)
     %Extract distance to transmitter
     z = zRange(m);
     
-    %Define the integrand E(x,y) in (5) and its absolute value square
+    %Define E(x,y) and its absolute value squared
     %(we have removed E_0/sqrt(4*pi) since it will cancel out)
     E_fun = @(x,y) sqrt((z.*(x.^2+z.^2))./((x.^2+y.^2+z.^2).^(5/2))).*exp(-1j*(2*pi).*sqrt((x.^2+y.^2+z.^2))/lambda);
     E2_fun = @(x,y) (z.*(x.^2+z.^2))./((x.^2+y.^2+z.^2).^(5/2));
     
     
-    %Compute all the terms in (15)
-    numerator_exact = zeros(Ndim,Ndim);
+   %%Array Gain
+    
+    %compute the integral in the demominator
     denominator_exact =  integral2(E2_fun, -D_antenna/sqrt(8),D_antenna/sqrt(8),-D_antenna/sqrt(8),D_antenna/sqrt(8));
+    
+    %compute all the terms in the numerator
+    numerator_exact = zeros(Ndim,Ndim);
+    
     
     for xdim = 1:Ndim
         
@@ -112,7 +117,6 @@ plot(relativeRange, arrayGain(:,4),'r--', 'Linewidth', 2);
 plot(relativeRange, arrayGain(:,3),'b-.', 'Linewidth', 2);
 plot(relativeRange, arrayGain(:,2),'c-', 'Linewidth', 2);
 plot(relativeRange, arrayGain(:,1),'g--', 'Linewidth', 2);
-plot(relativeRange, arrayGainFarfield,'m-.', 'Linewidth', 2);
 set(gca,'XScale','log');
 xticks([10 100 1000 10000 100000])
 xticklabels({'$10 d_F$','$10^2 d_F$','$10^3 d_F$','$10^4 d_F$','$10^5 d_F$'})
@@ -122,4 +126,4 @@ legend({'Maximum gain','$d_1=\infty$','$d_2=d_{FA}/20$','$d_3=d_{FA}/40$', '$d_4
 xlabel('Propagation distance','Interpreter','Latex');
 ylabel('Normalized antenna array gain','Interpreter','Latex');
 set(gca,'fontsize',18);
-%print('nearfield_multiplexing','-depsc','-r600')
+
